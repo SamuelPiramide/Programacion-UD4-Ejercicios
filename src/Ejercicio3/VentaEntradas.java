7056f22ce70f2826296fc58caa48a28c87cfe0b9
Samuel <230090@fppiramide.com>
2023-12-04 17:07:05 +0100

    Ejercicio 3 completado

--- src/Ejercicio3/VentaEntradas.java
+++ src/Ejercicio3/VentaEntradas.java
@@ -1,5 +1,5 @@
 package Ejercicio3;
-//Queremos gestionar la venta de entradas (no numeradas) de Expocoches Huesca que tiene 3 zonas:
+//Queremos gestionar la venta de entradas (no numeradas) de ExpoCoches Huesca que tiene 3 zonas:
 //        La zona de compra-venta con 200 entradas disponibles
 //        La zona vip con 25 entradas disponibles
 //        Hay que controlar que hay entradas disponibles antes de venderlas.
@@ -7,6 +7,8 @@ package Ejercicio3;
 import java.util.Scanner;
 
 public class VentaEntradas {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         Zona principal = new Zona(1000);
         Zona compraventa = new Zona(200);
@@ -15,7 +17,7 @@ public class VentaEntradas {
         int menu;
 
         do {
-            Scanner sc = new Scanner(System.in);
+            System.out.println("\n");
             System.out.println("1 - Mostrar número de entradas libres");
             System.out.println("2 - Vender entradas");
             System.out.println("3 - Salir");
@@ -23,19 +25,59 @@ public class VentaEntradas {
 
             switch (menu) {
                 case 1:
-                    System.out.println("Entradas por vender:");
-                    System.out.println("Zona principal: " + principal.getEntradasPorVender());
-                    System.out.println("Zona compra-venta: " + compraventa.getEntradasPorVender());
-                    System.out.println("Zona vip: " + vip.getEntradasPorVender());
+                    entradasPorVender(principal, compraventa, vip);
                     break;
 
                 case 2:
-                    
-                    System.out.println("¿Cuantas entradas quiere comprar?");
+                    compraEntradas(principal, compraventa, vip);
+                    break;
+
+                case 3:
+                    System.out.println("Hasta la vista.");
             }
         } while (menu != 3);
 
 
+    }
+
+    public static void entradasPorVender (Zona principal, Zona compraventa, Zona vip){
+        System.out.println("\n");
+        System.out.println("Entradas por vender:");
+        System.out.println("Zona principal: " + principal.getEntradasPorVender());
+        System.out.println("Zona compra-venta: " + compraventa.getEntradasPorVender());
+        System.out.println("Zona vip: " + vip.getEntradasPorVender());
+    }
+
+    public static void compraEntradas (Zona principal, Zona compraventa, Zona vip){
+        int numEntradas;
+        int menu;
+
+        System.out.println("¿Cuantas entradas quiere comprar?");
+        numEntradas = sc.nextInt();
+
+        System.out.println("¿En qué zona quiere comprar las entradas?");
+        System.out.println("1 - Zona principal");
+        System.out.println("2 - Zona compraventa");
+        System.out.println("3 - Zona vip");
+        menu = sc.nextInt();
+        switch (menu){
+
+            case 1:
+                principal.vender(numEntradas);
+                break;
+
+            case 2:
+                compraventa.vender(numEntradas);
+                break;
+
+            case 3:
+                vip.vender(numEntradas);
+                break;
+
+            default:
+                System.out.println("Esa no es una opción válida.");
+
+        }
     }
 
 }
