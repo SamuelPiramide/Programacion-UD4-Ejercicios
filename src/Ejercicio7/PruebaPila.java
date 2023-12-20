84560d4b21b478c5ab01dbc3c7d1b4b92f756e4d
Samuel <230090@fppiramide.com>
2023-12-20 09:29:01 +0100

    Ejercicio 7 acabado

--- src/Ejercicio7/PruebaPila.java
+++ src/Ejercicio7/PruebaPila.java
@@ -5,7 +5,29 @@ public class PruebaPila {
 
         PilaPalabras pila = new PilaPalabras();
 
+        String palabra1 = "Paco";
+        String palabra2 = "Roberto";
+        String palabra3 = "Oliver";
 
+
+
+        if(pila.pilaPlabrasVacia()) System.out.println("Esta vacia.");
+        else System.out.println("No está vacía.");
+
+        pila.apilarPalabra(palabra1);
+        pila.apilarPalabra(palabra2);
+        pila.apilarPalabra(palabra3);
+
+        if(pila.pilaPlabrasVacia()) System.out.println("Esta vacia.");
+        else System.out.println("No está vacía.");
+
+        System.out.println(pila.desapilarPalabra());
+
+        if(pila.pilaPlabrasVacia()) System.out.println("Esta vacia.");
+        else System.out.println("No está vacía.");
+
+        System.out.println(pila.obtenerPalabraCima());
+        }
 
     }
-}
+
