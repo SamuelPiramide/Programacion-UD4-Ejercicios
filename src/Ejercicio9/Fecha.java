1498e74aff38085f079bf4e486486803daa32875
Samuel <230090@fppiramide.com>
2023-12-23 14:35:01 +0100

    Arreglados getters y setters de fecha

--- src/Ejercicio9/Fecha.java
+++ src/Ejercicio9/Fecha.java
@@ -11,9 +11,39 @@ public class Fecha {
         this.year = year;
     }
 
+    public int getDay() {
+        return day;
+    }
+
     public void setDay(int day) {
         this.day = day;
     }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDiasemana() {
+        return diasemana;
+    }
+
+    public void setDiasemana(String diasemana) {
+        this.diasemana = diasemana;
+    }
+
+
 
     @Override
     public String toString() {
